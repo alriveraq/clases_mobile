2e81ab392cfc9db1a6096791225add8b8daca70c
Alejandr <aleparique@gmail.com>
2022-08-25 08:59:04 -0400

    clase de calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prueba';
+  public nombre : string = 'Alejandro';
+  public edad : number = 20 ;
+  public estaVivo : boolean = true ;
+  public apellido : string = 'Rivera';
+  public hobbie : string = 'Me gusta los videojuegos'
+
+  public sumarDosNumeros(n1: number, n2: number):number{
+    return n1 + n2 ;
+  }
+
+  public restar(n1: number, n2: number):number{
+    return n1 - n2 ;
+  }
+  public multiplicar(n1: number, n2: number):number{
+    return n1 * n2 ;
+  }
 }
